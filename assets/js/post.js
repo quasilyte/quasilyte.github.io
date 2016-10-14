4dc189af6535cfddfa0dab37bfb0d2bca55385c1
quasilyte <quasilyte@gmail.com>
2016-10-14 21:06:47 +0300

    more old browser-friendly way to display date string

--- assets/js/post.js
+++ assets/js/post.js
@@ -2,14 +2,15 @@
 
 (function() {
   App.postDateString = function(post) {
-    return (
-      new Date(post.date * 1000).
-      toLocaleString("en-us", {
-        "year": "numeric", 
-        "month": "2-digit", 
-        "day": "2-digit"
-      }).
-      replace(/(\d+)\/(\d+)\/(\d+)/, "$2.$1.$3")
-    );
+    var date = new Date(post.date * 1000);
+
+    var dd =   date.getDate();
+    var mm =   date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+
+    dd = dd < 10 ? `0${dd}` : dd;
+    mm = mm < 10 ? `0${mm}` : mm;
+
+    return `${dd}.${mm}.${yyyy}`;
   };
 }());
