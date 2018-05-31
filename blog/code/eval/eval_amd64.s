fc2417cf8fb73a7801ccb6a92c98c754169098f8
Iskander Sharipov <quasilyte@gmail.com>
2018-05-31 09:36:28 +0300

    go-asm-dispatch-tables: add code comments

---
+++ blog/code/eval/eval_amd64.s
@@ -0,0 +1,33 @@
+#include "textflag.h"
+
+DATA op_labels<>+0(SB)/8, $op_exit(SB)
+DATA op_labels<>+8(SB)/8, $op_add1(SB)
+DATA op_labels<>+16(SB)/8, $op_sub1(SB)
+DATA op_labels<>+24(SB)/8, $op_zero(SB)
+GLOBL op_labels<>(SB), (RODATA|NOPTR), $32
+
+#define next_op \
+  MOVBQZX (CX), DX \
+  ADDQ $1, CX \
+  MOVQ $op_labels<>(SB), DI \
+  JMP (DI)(DX*8)
+
+TEXT ·eval(SB), NOSPLIT, $0-16
+  MOVQ opbytes+0(FP), CX //; Set up program counter (PC)
+  next_op                //; Start the evaluation
+
+TEXT op_exit(SB), NOSPLIT, $0-0
+  MOVQ AX, ret+8(FP)
+  RET
+
+TEXT op_add1(SB), NOSPLIT, $0-0
+  ADDQ $1, AX
+  next_op
+
+TEXT op_sub1(SB), NOSPLIT, $0-0
+  SUBQ $1, AX
+  next_op
+
+TEXT op_zero(SB), NOSPLIT, $0-0
+  XORQ AX, AX
+  next_op
